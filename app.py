01821251a8ba6a9727683162c76cffcb29d0b544
ludovicleherisse <82951020+lehelud@users.noreply.github.com>
2024-10-10 07:19:03 +0000

    correction de bug

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import requests
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
 import geopandas as gpd
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 import numpy as np
 from babel.dates import format_date
 from pandas.tseries.holiday import USFederalHolidayCalendar as Calendar
@@ -16,25 +15,17 @@ plt.rcParams['font.family'] = 'Montserrat'
 # URL du fichier
 url = "https://www.data.gouv.fr/fr/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"
 
-
-# Télécharger le fichier
-# response = requests.get(url)
+# Fonction pour télécharger le CSV
 def download_csv(url, file_path):
     try:
-        # Send HTTP GET request to the URL
         response = requests.get(url)
-        
-        # Check if the request was successful (HTTP status code 200)
         if response.status_code == 200:
-            # Open a file in write-binary mode to save the CSV content
             with open(file_path, 'wb') as file:
                 file.write(response.content)
             print(f"File downloaded successfully and saved as {file_path}")
         else:
             print(f"Failed to download the file. Status code: {response.status_code}")
-    
     except requests.RequestException as e:
-        # Handle any exceptions that may occur during the request
         print(f"An error occurred: {e}")
 
 # Local path where the CSV will be saved
@@ -45,19 +36,18 @@ download_csv(url, file_path)
 
 # Lire le contenu téléchargé directement en DataFrame
 df = pd.read_csv(file_path, low_memory=False)
-# df = pd.read_csv(StringIO(response.text), low_memory=False)
 
 # Transformer toutes les colonnes date en datetime
 date_columns = df.filter(regex='date_|_at$').columns
 for col in date_columns:
-    df[col] = pd.to_datetime(df[col])
+    df[col] = pd.to_datetime(df[col], errors='coerce')  # Ajout de errors='coerce' pour éviter les erreurs sur les dates hors limites
 
 annee_actuelle = datetime.now().year
 vecteur_annee_inverse = np.arange(annee_actuelle, annee_actuelle - 1 - 7, -1)
-df["annee_mise_en_service"] = df.date_mise_en_service.dt.year.astype('Int64')
+df["annee_mise_en_service"] = pd.to_datetime(df['date_mise_en_service'], errors='coerce').dt.year.astype('Int64')
 
 # Compter le nombre unique de bornes installées par année
-df_year=df[df['annee_mise_en_service'].isin(vecteur_annee_inverse)]
+df_year = df[df['annee_mise_en_service'].isin(vecteur_annee_inverse)]
 
 # Calcul du nombre moyen d'installation par jour ouvré
 df_nb_id_year = df_year.groupby('annee_mise_en_service')['id_pdc_itinerance'].nunique()
@@ -69,7 +59,7 @@ results = pd.DataFrame(df_nb_id_year, columns=['id_pdc_itinerance'])
 workdays_per_year = {}
 years = results.index
 for year in years:
-    all_days = pd.date_range(start=str(year), end=str(year+1), inclusive='left', freq='B')
+    all_days = pd.date_range(start=str(year), end=str(year + 1), inclusive='left', freq='B')
     holidays = Calendar().holidays(start=all_days.min(), end=all_days.max())
     workdays = all_days.difference(holidays)
     workdays_per_year[year] = len(workdays)
@@ -89,29 +79,26 @@ if current_year in results.index:
     results.at[current_year, 'working_days'] = len(workdays)
     results.at[current_year, 'avg_per_working_day'] = results.at[current_year, 'id_pdc_itinerance'] / len(workdays)
 
-df_year['annee_mise_en_service'] = pd.to_datetime(df_year['date_mise_en_service']).dt.year
 bornes_par_annee = df_year.groupby('annee_mise_en_service')['id_pdc_itinerance'].nunique()
-
 
 # Création d'un GeoDataFrame pour les coordonnées des stations
 gdf_stations = gpd.GeoDataFrame(
     df,
     geometry=gpd.points_from_xy(df['consolidated_longitude'], df['consolidated_latitude']),
-    crs="EPSG:4326"  # Assurez-vous que c'est le bon système de référence
+    crs="EPSG:4326"
 )
 
-gdf_stations=gdf_stations[gdf_stations['annee_mise_en_service'].isin(vecteur_annee_inverse)]
+gdf_stations = gdf_stations[gdf_stations['annee_mise_en_service'].isin(vecteur_annee_inverse)]
 
 chemin_du_shapefile = "data/ARRONDISSEMENT.shp"
 
 # Lecture du shapefile avec geopandas
 france_metropolitaine = gpd.read_file(chemin_du_shapefile)
-
 france_metropolitaine = france_metropolitaine.to_crs(epsg=4326)
 
 # Définition des limites géographiques approximatives de la France métropolitaine
-latitude_min, latitude_max = 41.5, 51.0  # La Corse est approximativement au-dessus de 41.5° N
-longitude_min, longitude_max = -5.0, 9.5  # La Corse est à moins de 9.5° E
+latitude_min, latitude_max = 41.5, 51.0
+longitude_min, longitude_max = -5.0, 9.5
 
 # Filtrage des points en dehors des limites
 gdf_stations_france = gdf_stations[
@@ -120,62 +107,40 @@ gdf_stations_france = gdf_stations[
 ]
 
 # Fonction pour générer le graphique du nombre moyen de borne par année
-
 def plot_avg_per_working_day(results):
-    # Create a figure and an axes.
     fig, ax = plt.subplots(figsize=(10, 6))
-    # Plotting the bar chart
     ax.bar(results.index.astype(str), results['avg_per_working_day'], color='#007ACC', width=0.5)
     ax.set_title('Nombre moyen de bornes installées par jour ouvré par année')
     ax.set_xlabel('Année')
     ax.set_ylabel('Moyenne par jour ouvré')
-
-    # Supprimer le quadrillage et les axes pour une présentation épurée
     plt.grid(False)
     plt.box(False)
-
-    # Loop through the data points; add data labels for clarity
     for index, value in enumerate(results['avg_per_working_day']):
         ax.text(index, value, f'{value:.1f}', ha='center', va='bottom')
-    
-    # Apply a tight layout to manage spacing
     plt.tight_layout()
-    return fig  # Return the figure object to be used with Streamlit or any other display framework
-
+    return fig
 
 # Fonction pour générer le graphique du nombre total de borne par année
-
 def plot_bar_chart(df_year):
-
-    # Create figure and axis
     fig, ax = plt.subplots(figsize=(10, 6))
-
-    # Plotting the bar chart
     ax.bar(bornes_par_annee.index.astype(str), bornes_par_annee.values, color='#1AA6A6', width=0.5)
     ax.set_title('Nombre total de bornes installées par année')
     ax.set_xlabel('Année')
     ax.set_ylabel('Nombre de bornes installées')
-
-    # Supprimer le quadrillage et les axes pour une présentation épurée
     plt.grid(False)
     plt.box(False)
-
-    # Adding text labels above each bar for better readability
     for index, value in enumerate(bornes_par_annee.values):
         ax.text(index, value, f'{value:,.0f}'.replace(',', ' '), ha='center', va='bottom')
-
     plt.tight_layout()
-    return fig  # Return the figure object for use with Streamlit
+    return fig
 
-# Fonction pour générer le second graphique (Carte)
+# Fonction pour générer le graphique de la carte
 def plot_map(gdf_stations_france, france_metropolitaine):
     cmap = plt.cm.viridis
     norm = Normalize(vmin=gdf_stations_france['annee_mise_en_service'].min(), vmax=gdf_stations_france['annee_mise_en_service'].max())
-
     fig, ax = plt.subplots(figsize=(10, 10))
     france_metropolitaine.plot(ax=ax, color='lightgray')
     gdf_stations_france.plot(ax=ax, markersize=5, column='annee_mise_en_service', cmap=cmap, legend=False, norm=norm)
-
     sm = ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
     cbar = fig.colorbar(sm, ax=ax)
@@ -185,43 +150,34 @@ def plot_map(gdf_stations_france, france_metropolitaine):
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_title('Emplacement des stations de recharge en France')
-    # Use tight layout to ensure everything fits without overlapping
-    plt.tight_layout()    
-    return fig  # Return the figure object
+    plt.tight_layout()
+    return fig
 
-# Find the maximum date in the 'created_at' column
-date_max_created_at = df['created_at'].max()
+# Formater la date en français
+date_max_created_at = pd.to_datetime(df['created_at'], errors='coerce').max()
 next_day = date_max_created_at + timedelta(days=1)
-# Format the date in French
 formatted_date = format_date(next_day, format='long', locale='fr')
-
 
 # Streamlit UI
 def main():
     title = f"Bornes de recharge en France \n Dashboard en construction (données au {formatted_date} - mise à jour quotidienne)"
     st.title(title)
-
-    # Add a link to the data source
     st.markdown("Source des données: [Fichier consolidé des bornes de recharge pour véhicules électriques](https://www.data.gouv.fr/fr/datasets/fichier-consolide-des-bornes-de-recharge-pour-vehicules-electriques/)")
-    
-    # User choice for type of data visualization
     chart_type = st.radio(
         "Choisissez le type de graphique à afficher:",
         ('Nombre total de bornes installées par année', 'Nombre moyen de bornes installées par jour ouvré')
     )
-
     if chart_type == 'Nombre total de bornes installées par année':
         st.header("Nombre total de bornes installées par année")
-        fig1 = plot_bar_chart(df_year)  # Ensure this function is defined to plot total installations
-        st.pyplot(fig1.figure)
+        fig1 = plot_bar_chart(df_year)
+        st.pyplot(fig1)
     else:
         st.header("Nombre moyen de bornes installées par jour ouvré")
         fig2 = plot_avg_per_working_day(results)
-        st.pyplot(fig2.figure)
-
+        st.pyplot(fig2)
     st.header("Emplacement des stations de recharge en France")
-    fig3 = plot_map(gdf_stations_france, france_metropolitaine)  # Ensure this function is defined to plot the map
-    st.pyplot(fig3.figure)
+    fig3 = plot_map(gdf_stations_france, france_metropolitaine)
+    st.pyplot(fig3)
 
 if __name__ == "__main__":
     main()
